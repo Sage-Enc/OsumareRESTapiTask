d908ed1d484c90fb3397be3eaba36a4b6e903bb1
SAGE <rutveek21.rm@gmail.com>
2024-03-03 14:34:40 +0530

    Created A generateID function to generate RandomID

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+app.use(express.json());
+const fs = require("fs");
+const mm = require("moment");
+const port = 3000;
+const tasks = require("./task.js");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+// writeFile Funtion Write An Array To File
+const writeFile = ()=>{
+    fs.writeFileSync("./task.js", `module.exports = ${JSON.stringify(tasks)}`)
+} 
 
-app.get('/twitter', (req,res)=>{
-    res.send('<h1>Loafy_Sage</h1>')
+// generateId Function Generates A Random ID
+const generateId = (len) =>{
+    const result = "";
+    const chars = "0123456789qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
+    const charLen = chars.length;
+    for(let i=0; i<len; i++){
+        result += chars.charAt(Math.floor(Math.random()*charLen))
+    }
+    return result;
+}
+
+
+
+app.get("/", (req,res)=>{
+    res.send(data)
 })
 
 app.listen(port, () => {
